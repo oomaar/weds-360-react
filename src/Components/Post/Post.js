fbf87d994357f204e8d90447b5da254f5cb16a60
Omar <omaar_5@hotmail.com>
2021-02-05 18:49:00 +0200

    add: / pagination logic

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -8,26 +8,60 @@ import {
 
 const Post = () => {
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(9);
 
-    useEffect (() => {
+    useEffect(() => {
         const fetchPost = async () => {
+            setLoading(true);
             const res = await axios.get('https://jsonplaceholder.typicode.com/photos');
-            setPosts(res.data.slice(0, 3));
+            setPosts(res.data.slice(0, 100));
+            setLoading(false);
         }
 
         fetchPost();
     }, []);
     // console.log(posts);
+    loading && <h2>Spinner</h2>;
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPost = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+    // Pagination
+    const pageNumbers = [];
+
+    let i = 1;
+    while (i <= Math.ceil(posts.length / postsPerPage)) {
+        pageNumbers.push(i);
+        i++;
+    }
+
+    const paginate = (number) => setCurrentPage(number); 
+
     return (
         <Container>
             <Ulist>
-                {posts.map(post => (
+                {currentPost.map(post => (
                     <Item key={post.id}>
                         <img src={post.thumbnailUrl} alt={post.title} />
-                        <h6>{post.title}</h6>
+                        <h6>{post.id}</h6>
                     </Item>
                 ))}
             </Ulist>
+
+            <nav>
+                <ul>
+                    {pageNumbers.map(number => (
+                        <li key={number}>
+                            <a onClick={() => paginate(number)} href="!#">
+                                {number}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </Container>
     )
 }
